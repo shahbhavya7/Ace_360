4dae277c309ae98acadcb26c24e94c50c7b14849
shahbhavya7 <146073814+shahbhavya7@users.noreply.github.com>
2025-06-24 14:35:30 +0530

    toast update

--- components/hero.jsx
+++ components/hero.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useRef } from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-
+import {toast} from "sonner"
 const HeroSection = () => {
   const imageRef = useRef(null);
 
@@ -30,32 +30,37 @@ const HeroSection = () => {
     <section className="w-full pt-36 md:pt-48 pb-10">
       <div className="space-y-6 text-center">
         <div className="space-y-6 mx-auto text-center px-4">
-  <h1 className="text-5xl font-extrabold md:text-6xl lg:text-7xl xl:text-8xl 
+          <h1
+            className="text-5xl font-extrabold md:text-6xl lg:text-7xl xl:text-8xl 
                  bg-gradient-to-r from-blue-500 via-cyan-400 to-blue-300 
-                 bg-clip-text text-transparent animate-gradient-slow leading-tight">
-    Your AI Mentor for
-    <br />
-    Achieving Career Growth
-  </h1>
- <p className="mx-auto max-w-[620px] text-blue-200 md:text-lg">
-    Get expert support to land your dream role—AI-led coaching, smart tools, and tailored job strategies.
-  </p>
-</div>
+                 bg-clip-text text-transparent animate-gradient-slow leading-tight"
+          >
+            Your AI Mentor for
+            <br />
+            Achieving Career Growth
+          </h1>
+          <p className="mx-auto max-w-[620px] text-blue-200 md:text-lg">
+            Get expert support to land your dream role—AI-led coaching, smart
+            tools, and tailored job strategies.
+          </p>
+        </div>
 
         <div className="flex justify-center space-x-4">
           <Link href="/dashboard">
-            <Button size="lg"
-  className="px-8 bg-gradient-to-r from-cyan-400 to-blue-500 text-black font-semibold hover:brightness-110 transition">
+            <Button
+              size="lg"
+              className="px-8 bg-gradient-to-r from-cyan-400 to-blue-500 text-black font-semibold hover:brightness-110 transition"
+            >
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com/roadsidecoder">
-            <Button size="lg"
-     className="px-8 bg-[#0f172a] text-cyan-300 border border-cyan-500 hover:bg-cyan-900/40 transition"
->
-              Watch Demo
-            </Button>
-          </Link>
+          <Button
+            size="lg"
+            onClick={() => toast.warning("Coming soon!")}
+            className="px-8 bg-[#0f172a] text-cyan-300 border border-cyan-500 hover:bg-cyan-900/40 transition"
+          >
+            Watch Demo
+          </Button>
         </div>
         <div className="hero-image-wrapper mt-5 md:mt-0">
           <div ref={imageRef} className="hero-image">
